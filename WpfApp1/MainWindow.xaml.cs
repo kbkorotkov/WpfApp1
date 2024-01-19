ba60db0ce474cef1c0f879173ff73e5ac24d3d07
217013 <217013@edu.fa.ru>
2024-01-19 09:31:53 +0300

    gg

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
